936787254e54e26db1707f446fcc40500bbb2720
MaverickLegend <malcolmrojas.f@gmail.com>
2022-06-14 16:11:43 -0400

    entregafinaljs

--- main.js
+++ main.js
@@ -1,51 +1,66 @@
-let contactoForm = document.getElementById("formulario");
-contactoForm.addEventListener('submit', (e)=>{
+const contactoForm = document.getElementById("formulario");
+let formObj
+let consultas = []
+let checked = false;
+contactoForm.addEventListener('submit', (e) => {
     e.preventDefault();
-    let formularioPrueba = e.target
-    const datosForm = new FormData(document.getElementById("formulario"));
-    let nombre = datosForm.get('nombre_apellido');
-    let telefono = datosForm.get('numero_telefono')
-    let correo = datosForm.get('mail_mail')
-    let dominio = datosForm.get('mail_domain')
-    let servicio = datosForm.get('option_select')
-    let texto = datosForm.get('texto')
-    let fecha = datosForm.get('agenda')
-    fetch('https://jsonplaceholder.typicode.com/posts',{
-        method: 'POST',
-        body: JSON.stringify({
-            title: 'datosFormulario',
-            body: 'datosCliente',
-            userID: 1,
-            nombre_cliente: nombre,
-            telefono_cliente: telefono,
-            correo_cliente: correo,
-            dominio_cliente: dominio,
-            servicio_cliente: servicio,
-            consulta_cliente: texto,
-            fecha_cliente: fecha,
-        }),
-        headers: {
-            'contentType': 'application/json;charset=UTF-8',
-            },
-        })
-        .then((response) => response.json())
-        .then((res) => console.log(res))
-    console.log(formularioPrueba[0]. value)
-    console.log(formularioPrueba[1]. value)
-    console.log(formularioPrueba[2]. value, formularioPrueba[3]. value)
-    console.log(formularioPrueba[4]. value)
-    console.log(formularioPrueba[5]. value)
-    console.log(formularioPrueba[6]. value)
-    localStorage.setItem('nombre', formularioPrueba[0].value)
-    localStorage.setItem('telefono', formularioPrueba[1].value)
-    localStorage.setItem('mail', formularioPrueba[2].value)
-    localStorage.setItem('dominio', formularioPrueba[3].value)
-    localStorage.setItem('servicio', formularioPrueba[4].value)
-    localStorage.setItem('consulta', formularioPrueba[5].value)
-    localStorage.setItem('fecha', formularioPrueba[6].value)
-    let info = document.createTextNode("Muchas gracias, " + formularioPrueba[0].value +". Hemos recibido con éxito tu consulta.")
-    document.getElementById("infor").appendChild(info)
-})    
+    let datosForm = new FormData(document.getElementById("formulario"));
+
+    if(datosForm.get('nombre_apellido').trim() === ''){
+        console.log('nombre i apellido esta vacio');
+    }
+    else if(datosForm.get('mail_mail').trim() === ''){
+        console.log('mail esta vacio');
+    }
+    else if(datosForm.get('mail_domain').trim() === ''){
+        console.log('dominio de mail está vacío')
+    }
+    else if(datosForm.get('texto').trim() === ''){
+        console.log('su consulta está vacía')
+    }
+    else if(datosForm.get('agenda').trim() === ''){
+        console.log('no existe fecha')
+    }
+    else{
+        checked = true;
+    }
+
+    if (checked) {
+        const formularioPrueba = e.target
+        formObj = {
+            nombre: datosForm.get('nombre_apellido'),
+            telefono: datosForm.get('numero_telefono'),
+            correo: datosForm.get('mail_mail'),
+            dominio: datosForm.get('mail_domain'),
+            servicio: datosForm.get('servicio'),
+            texto: datosForm.get('texto'),
+            fecha: datosForm.get('agenda'),
+        }
+        localStorage.setItem('nombre', formularioPrueba[0].value)
+        localStorage.setItem('telefono', formularioPrueba[1].value)
+        localStorage.setItem('mail', formularioPrueba[2].value)
+        localStorage.setItem('dominio', formularioPrueba[3].value)
+        localStorage.setItem('servicio', formularioPrueba[4].value)
+        localStorage.setItem('consulta', formularioPrueba[5].value)
+        localStorage.setItem('fecha', formularioPrueba[6].value)
+        let info = document.createTextNode("Muchas gracias, " + formularioPrueba[0].value + ". Hemos recibido con éxito tu consulta.")
+        document.getElementById("infor").appendChild(info)
+        console.log(formObj)
+        consultas.push(formObj)
+        console.log(consultas)
+    }
+});
+
+const btnReset = document.getElementById("btnReset");
+btnReset.addEventListener('click', (e) => {
+    e.preventDefault();
+    document.getElementById('nombre').value = '';
+    document.getElementById('telefono').value = '';
+    document.getElementById('mail').value = '';
+    document.getElementById('domain').value = '';
+    document.getElementById('consulta').value = '';
+    document.getElementById('fecha').value = '';
+});
 
 document.getElementById("toast").addEventListener("click", () => {
     Toastify({
@@ -57,5 +72,4 @@ document.getElementById("toast").addEventListener("click", () => {
             background: "linear-gradient(to right, #00b09b, #96c93d)",
         }
     }).showToast();
-    })
-
+})
